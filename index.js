694d779a9681243ad48323955ea24cd649f41552
jaklahaulps <liu.perry@gmail.com>
2019-01-01 17:39:48 -0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let value = fromN;
+  if (toN === value){
+    return value;
+  }
+
+  return value + sum(value+1, toN)
+
 }
 
 module.exports = sum;
